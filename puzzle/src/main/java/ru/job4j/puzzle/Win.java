687a7732306a2ae21fa251c645c09f1a98e30e68
Ivan Kusurhashev <ivankusur@gmail.com>
2020-08-27 12:59:28 +0300

    Sokoban

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,8 +1,47 @@
 package ru.job4j.puzzle;
 
 public class Win {
-    public static boolean check(int[][] board) {
-        boolean rsl = true;
+
+    public static boolean monoHorizontal(int[][] matrix, int row) {
+        boolean result = true;
+        for (int cell = 0; cell < matrix[row].length; cell++) {
+            if (matrix[row][cell] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoVertical(int[][] matrix, int cell) {
+        boolean result = true;
+        for (int[] chars : matrix) {
+            if (chars[cell] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static int[] extractDiagonal(int[][] board) {
+        int[] rsl = new int[board.length];
+        for (int i = 0; i < board.length; i++) {
+            rsl[i] = board[i][i];
+        }
         return rsl;
+    }
+
+    public static boolean check(int[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (extractDiagonal(board)[i] == 1) {
+                if (monoHorizontal(board, i) || monoVertical(board, i)) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
     }
 }
